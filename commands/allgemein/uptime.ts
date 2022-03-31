cf1298e58b9fcc3a9fca625ab091f8bf58bfbbfd
TigerbyteDev <tigerbytedev@gmail.com>
2022-03-31 16:49:36 +0200

    deleted ping, created uptime

---
+++ commands/allgemein/uptime.ts
@@ -0,0 +1,14 @@
+import { commandFile } from "../../types.ts"
+import { ms } from "../../deps.ts"
+
+export const commandSetup: commandFile = {
+    name: "uptime",
+    description: "Pong halt",
+
+    run: async (interaction, client): Promise<void> => {
+        await interaction.reply(`Online seit ${ms(client?.uptime ?? 0)}`, {
+            ephemeral: true
+        })
+        return;
+    }
+}
