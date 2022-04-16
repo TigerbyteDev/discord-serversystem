07179f16c4989651cae7c9f05942836ebcff4db1
TigerbyteDev <tigerbytedev@gmail.com>
2022-04-16 19:46:35 +0200

    Added Uptime Transferred Calculator, not fixed yet

---
+++ src/commands/allgemeini/uptime.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "uptime",
+    description: "Zeigt, wie lang der Bot bereits online ist",
+
+    run: async (interaction, client, tdhandler) => {
+        // format the uptime
+        const uptime = client.uptime,
+            days = `**${Math.floor(uptime / 86400000)} Tage**`,
+            hours = `**${Math.floor((uptime % 86400000) / 3600000)} Stunden**`,
+            minutes = `**${Math.floor((uptime % 3600000) / 60000)} Minuten**`,
+            seconds = `**${Math.floor((uptime % 60000) / 1000)} Sekunden**`;
+
+        // send the uptime
+        await interaction.reply({
+            embeds: [
+                await tdhandler.createEmbed("default")
+                .setTitle(":clock: Uptime")
+                .setDescription(`${client.user.username} ist online seit ${days}, ${hours}, ${minutes} und ${seconds}`)
+            ]
+        })
+    }
+}
