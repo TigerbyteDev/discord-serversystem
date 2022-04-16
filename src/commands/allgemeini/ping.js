52c12c4c67fa0b5f02e6509b28c6409447354e61
TigerbyteDev <tigerbytedev@gmail.com>
2022-04-16 18:58:08 +0200

    ping and poll command

--- src/commands/allgemeini/ping.js
+++ src/commands/allgemeini/ping.js
@@ -2,7 +2,21 @@ module.exports = {
     name: "ping",
     description: "Erhalte den Botping",
 
-    run: async (interaction) => {
-        interaction.reply("pong")
+    run: async (interaction, client, tdhandler) => {
+        await interaction.reply({
+            embeds: [
+                await tdhandler.createEmbed("default")
+                .setTitle(":ping_pong: Pong!")
+                .addFields({
+                    "name": 'API Verzögerung',
+                    "value": `\`${client.ws.ping}ms\``,
+                    "inline": true
+                }, {
+                    "name": 'Client Verzögerung',
+                    "value": `\`${Date.now() - interaction.createdTimestamp}ms\``,
+                    "inline": true
+                })
+            ]
+        })
     }
 }
