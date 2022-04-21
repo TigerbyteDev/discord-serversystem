4b493b1e638d25812276006ba87a2e02611f6e7c
adabr <tigerbytedev@gmail.com>
2022-04-21 15:24:54 +0200

    added tensorflow toxic filter

---
+++ src/events/messageCreate.js
@@ -0,0 +1,28 @@
+const toxicity = require('@tensorflow-models/toxicity');
+
+module.exports = {
+    name: "messageCreate",
+    once: false,
+
+    run: async message => {
+        const client = tdhandler.getClient;
+        if (message.author.id === client.user.id) return false;
+
+        const model = await toxicity.load(0.6, ["identity_attack", "insult", "obscene", "severe_toxicity", "sexual_explicit", "threat", "toxicity"]);
+        const predictions = await model.classify(message.content);
+        console.log(predictions);
+        for (const e of predictions) {
+            if (e.results[0].match) {
+                await message.reply({
+                    content: `Deine Nachricht wird gelöscht, weil sie ein Toxic-level von **${
+                        e.results[0].match ? Math.round(e.results[0].probabilities[1] * 100) : Math.round(e.results[0].probabilities[0] * 100)
+                    }%** hat.`
+                });
+                message.delete();
+                tdhandler.log(`${message.author.tag} hat eine Nachricht gesendet mit einem Toxic-level von **${
+                    e.results[0].match ? Math.round(e.results[0].probabilities[1] * 100) : Math.round(e.results[0].probabilities[0] * 100)
+                }%**.`);
+            }
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -8,50 +8,32 @@ const client = new Client({
 
 new TDInstance({
     handling: {
-        // The directory the other paths are oriented to
         baseDir: __dirname,
-        // The directory your events are located in
-        // eventsDir: "./events",
-        // The directory your commands are located in
+        eventsDir: "./events",
         commandsDir: "./commands",
-        // The directory your buttons are located in
         buttonsDir: "./buttons",
-        // The directory your context menus are located in
         // contextDir: "./context",
     },
     logging: {
-        // The ID of the channel your events should be logged in
-        eventsID: "955531040330498091",
-        // The ID of the channel your commands should be logged in
         commandsID: "955531040330498091",
-        // The ID of the channel your buttons should be logged in
         buttonsID: "955531040330498091",
-        // The ID of the channel your context menus should be logged in
-        contextID: "955531040330498091",
-        // The ID of the channel everything other should be logged in
+        // contextID: "955531040330498091",
         othersID: "955531040330498091"
     },
     embeds: {
-        // The options for different default-embeds
         warningEmbed: {
             color: "RED",
-            title: "warning",
-            footer: "warn",
-            footerIcon: "https://de.wikipedia.org/wiki/Datei:Ia-never-gonna-give-you-up-rick-astley-trionfale-remaster-4k-v3-500421.jpg",
-            timestamp: false,
+            title: "Achtung! ⚠️",
         },
         defaultEmbed: {
             color: "BLUE",
-            footer: "default",
-            footerIcon: "https://de.wikipedia.org/wiki/Datei:Ia-never-gonna-give-you-up-rick-astley-trionfale-remaster-4k-v3-500421.jpg",
-            timestamp: false,
+            timestamp: true,
         },
         loggingEmbed: {
             color: "YELLOW",
-            timestamp: false,
+            timestamp: true,
         },
     },
-    // The members of your team
     team: [
         {
             tag: "Adam ^^#7729",
@@ -60,9 +42,7 @@ new TDInstance({
         },
     ],
     testing: {
-        // The ID of your testbot
         botID: "803017957528698913",
-        // The ID of your testguild
         guildID: "751938789412175974",
     }
 })
