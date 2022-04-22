0bd359405c8a2722a6b0c7a49d782a1480886cbe
adabr <tigerbytedev@gmail.com>
2022-04-22 11:05:24 +0200

    removed tensorflow filter due to performance issues

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -8,21 +8,23 @@ module.exports = {
         const client = tdhandler.getClient;
         if (message.author.id === client.user.id) return false;
 
-        const model = await toxicity.load(0.6, ["identity_attack", "insult", "obscene", "severe_toxicity", "sexual_explicit", "threat", "toxicity"]);
-        const predictions = await model.classify(message.content);
-        console.log(predictions);
-        for (const e of predictions) {
-            if (e.results[0].match) {
-                await message.reply({
-                    content: `Deine Nachricht wird gelöscht, weil sie ein Toxic-level von **${
+        async function toxicCheck() {
+            const model = await toxicity.load(0.6, ["identity_attack", "insult", "obscene", "severe_toxicity", "sexual_explicit", "threat", "toxicity"]);
+            const predictions = await model.classify(message.content);
+            console.log(predictions);
+            for (const e of predictions) {
+                if (e.results[0].match) {
+                    await message.reply({
+                        content: `Deine Nachricht wird gelöscht, weil sie ein Toxic-level von **${
+                            e.results[0].match ? Math.round(e.results[0].probabilities[1] * 100) : Math.round(e.results[0].probabilities[0] * 100)
+                        }%** hat.`
+                    });
+                    message.delete();
+                    tdhandler.log(`${message.author.tag} hat eine Nachricht gesendet mit einem Toxic-level von **${
                         e.results[0].match ? Math.round(e.results[0].probabilities[1] * 100) : Math.round(e.results[0].probabilities[0] * 100)
-                    }%** hat.`
-                });
-                message.delete();
-                tdhandler.log(`${message.author.tag} hat eine Nachricht gesendet mit einem Toxic-level von **${
-                    e.results[0].match ? Math.round(e.results[0].probabilities[1] * 100) : Math.round(e.results[0].probabilities[0] * 100)
-                }%**.`);
-                break;
+                    }%**.`);
+                    break;
+                }
             }
         }
     }
